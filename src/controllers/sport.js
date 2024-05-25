d277ea86c4558c5b47dd1d6b39d43c3fccef0b89
vishnukant7 <vishnukant.trivedi@zillskill.com>
2024-05-25 13:18:32 +0530

    Problem 2 - endpoint /sport/tour/match now return match's id, startTime and format

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,15 +4,21 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchId, matchName, startTime, format } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({
+            id: matchId,
+            name: matchName,
+            startTime,
+            format
+        });
     });
+
     return res;
 }
 
