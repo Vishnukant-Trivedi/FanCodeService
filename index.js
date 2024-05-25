3dc4b4dfa0333a4523d8a40d913e53fc82dd0968
vishnukant7 <vishnukant.trivedi@zillskill.com>
2024-05-25 14:57:15 +0530

    Problem 3 - added middleware to accept the req.body

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const config = require('./config/config');
 const app = express();
 const port = config.server.port;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 //Register routes
 helper
     .fileList('./src/routes')
